65b8c072728f7d5b45844e673429864d07e1dd98
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2009-07-03 17:03:27 +0200

    Added the RevolverSound class for playing multiple sounds.

---
+++ Source/RevolverSound.h
@@ -0,0 +1,22 @@
+#import "Sound.h"
+
+/*
+    When you send the “play” message to a regular sound
+    (instance of Sound.m), the sound rewinds and plays from
+    the start. This is not always desirable, a lot of times
+    you would like to play many instances of the same sound
+    simultaneously (think machinegun). This is what this
+    class is for.
+*/
+@interface RevolverSound : NSObject
+{
+    NSMutableArray *sounds;
+    int current;
+}
+
+- (id) initWithFile: (NSString*) file rounds: (int) max;
+
+- (void) play;
+- (void) stop;
+
+@end
---
+++ Source/RevolverSound.m
@@ -0,0 +1,32 @@
+#import "RevolverSound.h"
+#import "Sound+IO.h"
+
+@implementation RevolverSound
+
+- (id) initWithFile: (NSString*) file rounds: (int) max
+{
+    [super init];
+    sounds = [[NSMutableArray alloc] init];
+    for (int i=0; i<max; i++)
+        [sounds addObject:[[[Sound alloc] initWithFile:file] autorelease]];
+    return self;
+}
+
+- (void) dealloc
+{
+    [sounds release];
+    [super dealloc];
+}
+
+- (void) play
+{
+    [[sounds objectAtIndex:current] play];
+    current = (current + 1) % [sounds count];
+}
+
+- (void) stop
+{
+    [[sounds objectAtIndex:current] stop];
+}
+
+@end
