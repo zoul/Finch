6db314d06f32a4e66c8fe22ec2348de406d286fb
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2011-09-09 14:13:20 +0200

    Deleted some extra comments.

--- Finch/FISound.h
+++ Finch/FISound.h
@@ -2,15 +2,10 @@
 
 @interface FISound : NSObject
 
-// Sound length in seconds.
 @property(readonly) float duration;
-// Is the sound currently playing?
 @property(readonly) BOOL playing;
-// Should the sound loop?
 @property(assign, nonatomic) BOOL loop;
-// Sound gain.
 @property(assign, nonatomic) float gain;
-// Sound pitch.
 @property(assign, nonatomic) float pitch;
 
 - (id) initWithSample: (FISoundSample*) sample error: (NSError**) error;
