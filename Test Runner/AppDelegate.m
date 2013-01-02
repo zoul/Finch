a697cd962e4a9c092a33898a2859f48d42ecdc7e
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2013-01-02 20:11:26 +0100

    Simple status bar while running the tests.

--- Test Runner/AppDelegate.m
+++ Test Runner/AppDelegate.m
@@ -3,9 +3,25 @@
 @implementation AppDelegate
 @synthesize window;
 
+- (UIWindow*) buildMainWindow
+{
+    CGRect screenFrame = [[UIScreen mainScreen] bounds];
+    UIWindow *win = [[UIWindow alloc] initWithFrame:screenFrame];
+    [win setBackgroundColor:[UIColor whiteColor]];
+
+    UILabel *label = [[UILabel alloc] init];
+    [label setText:@"Running Tests…"];
+    [label sizeToFit];
+
+    [win addSubview:label];
+    [label setCenter:CGPointMake(screenFrame.size.width/2, 100)];
+
+    return win;
+}
+
 - (BOOL) application: (UIApplication*) application didFinishLaunchingWithOptions: (NSDictionary*) launchOptions
 {
-    [self setWindow:[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]]];
+    [self setWindow:[self buildMainWindow]];
     [window makeKeyAndVisible];
     return YES;
 }
