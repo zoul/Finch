cc355ed6fb2591f022332368d9647738c075d423
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2013-01-05 09:19:35 +0100

    Basic high-level sound loading tests.

---
+++ Finch/FISoundTests.m
@@ -0,0 +1,25 @@
+#import "FITestCase.h"
+#import "FISound.h"
+
+@interface FISoundTests : FITestCase
+@end
+
+@implementation FISoundTests
+
+- (void) testInitializationWithNilPath
+{
+    FISound *sound = nil;
+    STAssertNoThrow(
+        sound = [[FISound alloc] initWithPath:nil error:NULL],
+        @"Do not throw when trying to load a sound from a nil path");
+    STAssertNil(sound, @"Return nil when loading a sound from a nil path");
+}
+
+- (void) testBasicLoading
+{
+    NSString *path = [[NSBundle bundleForClass:[self class]] pathForResource:@"mono8bit" ofType:@"wav"];
+    FISound *sound = [[FISound alloc] initWithPath:path error:NULL];
+    STAssertNotNil(sound, @"Load a basic sound");
+}
+
+@end
