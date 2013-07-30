6f2b416ffe2d5b24945e7cd0cdbb41898d90d1fc
= <adrian@uken.com>
2013-07-30 10:28:28 -0400

    stopped using alBufferDataStatic because it leads to bugs

--- Finch/FISampleBuffer.m
+++ Finch/FISampleBuffer.m
@@ -1,13 +1,13 @@
 #import "FISampleBuffer.h"
 #import "FIError.h"
 
-typedef ALvoid AL_APIENTRY (*alBufferDataStaticProcPtr) (const ALint bid,
-ALenum format,
-const ALvoid* data,
-ALsizei size,
-ALsizei freq);
-
-static alBufferDataStaticProcPtr alBufferDataStatic = NULL;
+//typedef ALvoid AL_APIENTRY (*alBufferDataStaticProcPtr) (const ALint bid,
+//ALenum format,
+//const ALvoid* data,
+//ALsizei size,
+//ALsizei freq);
+//
+//static alBufferDataStaticProcPtr alBufferDataStatic = NULL;
 
 @implementation FISampleBuffer
 
@@ -22,7 +22,6 @@ static alBufferDataStaticProcPtr alBufferDataStatic = NULL;
         _sampleFormat = sampleFormat;
         _sampleRate = sampleRate;
         _numberOfSamples = dataSize / [self bytesPerSample];
-        //theData = data;
     } else {
         return nil;
     }
@@ -48,11 +47,17 @@ static alBufferDataStaticProcPtr alBufferDataStatic = NULL;
 
     alClearError();
     
-    theData = data;
-    //alBufferDataStatic (as opposed to alBufferData) points to the passed in data, rather than copy it.
-    alBufferDataStatic = (alBufferDataStaticProcPtr) alcGetProcAddress(NULL, (const ALCchar*) "alBufferDataStatic");
-    alBufferDataStatic(_handle, [self OpenALSampleFormat], data, dataSize, sampleRate);
-    //free(data); //only needed on alBufferData, on alBufferDataStatic, we call free(data) in the dealloc.
+    // iOS appears to have issues with alBufferDataStatic, we'll avoid using it to be safe
+    // http://books.google.ca/books?id=QoxeAqTvevIC&pg=PA509&lpg=PA509&dq=alBufferDataStatic&source=bl&ots=fLRdN_PBe4&sig=S9xFWvwSQVx4Qa1tXTBWw-PRTK0&hl=en&sa=X&ei=Hcf2UZeIO5be4APk5ICADA&ved=0CEEQ6AEwAw#v=onepage&q=alBufferDataStatic&f=false
+    // http://benbritten.com/2009/09/20/albufferdatastatic-why-you-should-avoid-it/
+//    theData = data;
+//    //alBufferDataStatic (as opposed to alBufferData) points to the passed in data, rather than copy it.
+//    alBufferDataStatic = (alBufferDataStaticProcPtr) alcGetProcAddress(NULL, (const ALCchar*) "alBufferDataStatic");
+//    alBufferDataStatic(_handle, [self OpenALSampleFormat], data, dataSize, sampleRate);
+//    //free(data); //only needed on alBufferData, on alBufferDataStatic, we call free(data) in the dealloc.
+    
+    alGenBuffers(1, &_handle);
+    alBufferData(_handle, [self OpenALSampleFormat], data, dataSize, sampleRate);
     
     status = alGetError();
     if (status) {
@@ -69,7 +74,6 @@ static alBufferDataStaticProcPtr alBufferDataStatic = NULL;
     if (_handle) {
         alDeleteBuffers(1, &_handle);
         _handle = 0;
-        free(theData);
     }
 }
 
